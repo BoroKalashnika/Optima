7877f8891b71a77b4c7ba525c42470e6b89a7585
GodoyJuanma <godoyjuanma781@gmail.com>
2025-02-07 22:01:24 +0100

    Error Ionicons

--- src/Screens/Home/HomeNavegacion.js
+++ src/Screens/Home/HomeNavegacion.js
@@ -1,17 +1,49 @@
-import {
-    View,
-    StyleSheet,
-} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Home from './Home';
+import Buscar from './Buscar';
+import CrearRutinaNavegacion from '../CrearRutina/CrearRutinaNavegacion';
+import FavoritosNavegacion from '../Favoritos/FavoritosNavegacion';
+
+const Tab = createBottomTabNavigator();
 
 const HomeNavegacion = (props) => {
     return (
-        <View style={styles.container}>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Home" component={Home} options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" size={size} color={color} />
+                    ), headerShown: false
+                }}
+            />
+            <Tab.Screen
+                name="Buscar" component={Buscar} options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="search" size={size} color={color} />
+                    ),
+                    headerShown: false
+                }}
+            />
+            <Tab.Screen
+                name="CrearRutinaNavegacion" component={CrearRutinaNavegacion} options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="add" size={size} color={color} />
+                    ),
+                    headerShown: false
+                }}
+
+            />
+            <Tab.Screen
+                name="FavoritosNavegacion" component={FavoritosNavegacion} options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="heart" size={size} color={color} />
+                    ),
+                    headerShown: false
+                }}
+            />
+        </Tab.Navigator>
     );
 }
-
-const styles = StyleSheet.create({
-
-});
 
 export default HomeNavegacion;
