c931959d58ec733c6ee52b2c00ccf8a6153d78bc
GodoyJuanma <godoyjuanma781@gmail.com>
2025-02-07 23:00:33 +0100

    Home Home

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,17 +1,18 @@
-import {
-    View,
-    StyleSheet,
-} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Usuario from './Usuario';
+import CalcularIMC from "./CalcularIMC";
+import CalcularMacros from "./CalcularMacros"
+// import Ajustes from '../VerEjercicio';
 
-const Home = (props) => {
-    return (
-        <View style={styles.container}>
-        </View>
-    );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-
-});
+const Home = () => (
+    <Stack.Navigator options="headerShown=false">
+        <Stack.Screen name="Usuario" component={Usuario} options={{ headerShown: false }} />
+        <Stack.Screen name="CalcularIMC" component={CalcularIMC} options={{ headerShown: false }} />
+        <Stack.Screen name="CalcularMacros" component={CalcularMacros} options={{ headerShown: false }} />
+        {/* <Stack.Screen name="Ajustes" component={Ajustes} options={{ headerShown: false }} /> */}
+    </Stack.Navigator>
+);
 
 export default Home;
