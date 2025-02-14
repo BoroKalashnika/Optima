a4db9c30ba372d9f740e6eb9c20c2a7f0c9490a3
javi <javi@socorro.com>
2025-02-14 17:01:44 +0100

    ajustes acabado

--- src/Screens/Ajustes.js
+++ src/Screens/Ajustes.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import Context from '../Utils/Context';
-import { View, Text, Image, ScrollView, StyleSheet, Alert, Pressable } from 'react-native';
+import { View, Text, Image, ScrollView, StyleSheet, Alert, Pressable, Switch, TextInput } from 'react-native';
 import HeaderRutina from '../Components/headerRutina/HeaderRutina';
 import Icon from 'react-native-vector-icons/AntDesign';
 import postData from '../Utils/services/postData';
@@ -20,15 +20,22 @@ const Ajustes = (props) => {
     const [nombre, setNombre] = useState("nombre");
     const [vistaPrevia, setVistaPrevia] = useState(null);
     const [foto, setFoto] = useState();
+    const [isDarkMode, setIsDarkMode] = useState(true); // Estado para el modo oscuro/claro
+    const [nombreEditable, setNombreEditable] = useState(false);
+    const [nuevoNombre, setNuevoNombre] = useState(nombre); // Inicializa nuevoNombre con el valor de nombre
 
     useEffect(() => {
         getData(config.API_OPTIMA + 'tokenUsuario?token=' + token).then((response) => {
             setNombre(response.nombre);
+            setNuevoNombre(response.nombre); // Actualiza nuevoNombre con el nombre del usuario
             if (response.fotoPerfil !== "") {
                 setFoto(response.fotoPerfil);
             }
         });
     }, []);
+
+    // Elimina este useEffect, ya que no es necesario
+    // useEffect(() => { setNuevoNombre(nombre) }, [nombre]);
 
     useEffect(() => {
         const actualizarFoto = async () => {
@@ -52,10 +59,9 @@ const Ajustes = (props) => {
                 setVistaPrevia(true);
             }
         };
-    
+
         actualizarFoto();
     }, [foto]);
-    
 
     const cerrarSesion = async () => {
         setLoading(true);
@@ -104,40 +110,92 @@ const Ajustes = (props) => {
         });
     };
 
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
 
+    const toggleEditNombre = async () => {
+        if (nombreEditable) {
+            setLoading(true);
+            const json = {
+                token: token,
+                nuevoNombre: nuevoNombre,
+            };
+            console.log(nuevoNombre);
+            try {
+                const response = await postData(config.API_OPTIMA + 'cambiarNombre', json, setLoading);
+                if (response.status === 200) {
+                    setNombre(nuevoNombre);  // Actualizamos el nombre en el estado local
+                    setAlertMessage("Nombre actualizado correctamente");
+                    setAlertTitle("Éxito");
+                    setModalVisible(true);
+                } else {
+                    console.error("Error en la respuesta de la API:", response);
+                    Alert.alert("ERROR", response.data?.message || "No se pudo actualizar el nombre");
+                }
+            } catch (error) {
+                console.error("Error al actualizar el nombre:", error);
+                Alert.alert("ERROR", "Hubo un problema al comunicarse con el servidor");
+            }
+
+            setLoading(false);
+        }
+        setNombreEditable(!nombreEditable);
+    };
     if (loading) {
-        return (
-            <Carga />
-        );
+        return <Carga />;
     }
+
     return (
-        <ScrollView contentContainerStyle={styles.container}>
+        <ScrollView contentContainerStyle={[styles.container, isDarkMode ? styles.darkMode : styles.lightMode]}>
             <HeaderRutina tipo={'user'} />
             <View style={styles.profileContainer}>
                 <Pressable onPress={() => cambairFoto()}>
-                    {vistaPrevia ? <Image source={{ uri: foto }} style={styles.profileImage} /> : (
+                    {vistaPrevia ?
                         <>
-                            <Image source={require('../Assets/img/perfil.png')} style={styles.profileImage} />
+                            <Image source={{ uri: foto }} style={styles.profileImage} />
                             <View style={styles.editIcon}>
                                 <Icon name="form" size={35} color="#607cff" />
                             </View>
-                        </>
+                        </> : (
+                            <>
+                                <Image source={require('../Assets/img/perfil.png')} style={styles.profileImage} />
+                                <View style={styles.editIcon}>
+                                    <Icon name="form" size={35} color="#607cff" />
+                                </View>
+                            </>
+                        )}
+                </Pressable>
+            </View>
+
+            <View style={styles.profileContainer}>
+                <View style={styles.editContainer}>
+                    {nombreEditable ? (
+                        <TextInput
+                            style={[styles.input, isDarkMode ? styles.darkInput : styles.lightInput]}
+                            value={nuevoNombre}
+                            onChangeText={setNuevoNombre}
+                        />
+                    ) : (
+                        <Text style={[styles.name, isDarkMode ? styles.darkText : styles.lightText]}>{nombre}</Text>
                     )}
-                </Pressable>
+                    <Pressable style={styles.editButton} onPress={toggleEditNombre}>
+                        <Text style={styles.editButtonText}>{nombreEditable ? 'Guardar' : 'Modificar'}</Text>
+                    </Pressable>
+                </View>
             </View>
 
-            <Text style={styles.name}>{nombre}</Text>
-
-            <Text style={styles.languageText}>Idioma de la aplicación</Text>
-            <View style={styles.flagsContainer}>
-                <Pressable>
-                    <Image source={require('../Assets/img/spain.png')} style={styles.flag} />
-                </Pressable>
-                <Pressable>
-                    <Image source={require('../Assets/img/english.png')} style={styles.flag} />
-                </Pressable>
+            <View style={styles.themeContainer}>
+                <Text style={[styles.themeText, isDarkMode ? styles.darkText : styles.lightText]}>Modo Oscuro</Text>
+                <Switch
+                    value={isDarkMode}
+                    onValueChange={toggleDarkMode}
+                    thumbColor={isDarkMode ? '#607cff' : '#f4f3f4'}
+                    trackColor={{ false: '#767577', true: '#81b0ff' }}
+                />
             </View>
-            <Pressable style={styles.logoutButton} onPress={() => cerrarSesion()}>
+
+            <Pressable style={[styles.logoutButton, isDarkMode ? styles.darkButton : styles.lightButton]} onPress={() => cerrarSesion()}>
                 <Text style={styles.logoutText}>Cerrar sesión</Text>
             </Pressable>
         </ScrollView>
@@ -149,10 +207,38 @@ const styles = StyleSheet.create({
         flexGrow: 1,
         alignItems: 'center',
         padding: 20,
+    },
+    input: {
+        borderRadius: 10,
+        marginBottom: 10,
+        width: '60%',
+        backgroundColor: '#374151',
+        color: '#bbdefb',
+        padding: 12,
+        borderColor: '#607cff',
+        borderWidth: 1,
+    },
+    darkMode: {
         backgroundColor: '#1F2937',
+    },
+    lightMode: {
+        backgroundColor: '#ffffff',
+    },
+    darkText: {
+        color: '#bbdefb',
+    },
+    lightText: {
+        color: '#1F2937',
     },
     profileContainer: {
         position: 'relative',
+        marginTop: 50
+    },
+    editContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+        gap: 20
     },
     profileImage: {
         width: 185,
@@ -168,46 +254,57 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         padding: 5,
     },
-    icon: {
-        width: 20,
-        height: 20,
-    },
     name: {
         fontSize: 40,
         fontWeight: 'bold',
-        color: '#bbdefb',
         marginTop: 10,
     },
     languageText: {
         fontSize: 25,
-        color: 'white',
         marginTop: 20,
         marginBottom: 10,
     },
-    flagsContainer: {
+    themeContainer: {
         flexDirection: 'row',
-        marginTop: 10,
-        gap: 15
+        alignItems: 'center',
+        marginTop: 40,
+        gap: 10,
     },
-    flag: {
-        width: 100,
-        height: 55,
-        borderRadius: 5,
+    themeText: {
+        fontSize: 20,
     },
     logoutButton: {
         position: 'absolute',
         bottom: 20,
         right: 20,
-        backgroundColor: '#FF3B30',
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 10,
+    },
+    darkButton: {
+        backgroundColor: '#FF3B30',
+    },
+    lightButton: {
+        backgroundColor: '#607cff',
     },
     logoutText: {
         fontSize: 18,
         color: 'white',
         fontWeight: 'bold',
-    }
+    },
+    editButton: {
+        padding: 8,
+        backgroundColor: '#607cff',
+        borderRadius: 5,
+        height: 45,
+        width: 90,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    editButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
 });
 
 export default Ajustes;
